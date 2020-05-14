6ee32efbbf2a7855e2cbdfe3602ffa26b23866ca
Trevor Spires <trevor.spires1@gmail.com>
2020-05-14 18:36:25 -0400

    Update variables.tf

--- 3tierapp/variables.tf
+++ 3tierapp/variables.tf
@@ -1,18 +1,31 @@
 #Variables
 
+#vCenter
+variable "vsphere_server" {
+  default = "vcsa-01a.corp.local"
+}
+
+# Username & Password for vCenter
+variable "vsphere_user" {
+  default = "administrator@vsphere.local"
+}
+variable "vsphere_password" {
+    default = "VMware1!"
+}
+
+#NSX Manager
 variable "nsx_manager" {
   default = "nsxmgr-01a"
 }
- 
+
 # Username & Password for NSX-T Manager
-variable "username" {
+variable "nsx_username" {
   default = "admin"
 }
- 
-variable "password" {
+variable "nsx_password" {
     default = "VMware1!VMware1!"
 }
- 
+
 # Segment Names
 variable "nsx_segment_web" {
     default = "TF-Segment-Web"
@@ -20,24 +33,23 @@ variable "nsx_segment_web" {
 variable "nsx_segment_app" {
     default = "TF-Segment-App"
 }
- 
 variable "nsx_segment_db" {
     default = "TF-Segment-DB"
 }
- 
+
 # Security Group names.
 variable "nsx_group_web" {
-    default = "Web Servers"
+    default = "TF Web Servers"
 }
- 
+
 variable "nsx_group_app" {
-    default = "App Servers"
+    default = "TF App Servers"
 }
- 
+
 variable "nsx_group_db" {
-    default = "DB Servers"
+    default = "TF DB Servers"
 }
- 
-variable "nsx_group_blue" {
-    default = "Blue Application"
+
+variable "nsx_group_three_tier_app" {
+    default = "TF three tier app"
 }
